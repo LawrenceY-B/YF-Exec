fda3374d0c39176490d294d457907fab76d7b9e4
Lawrence Yirenkyi-Boafo <yirenkyi-boafol@stanbic.com.gh>
2025-10-10 11:40:04 +0000

    feat: add @vercel/analytics package and integrate Analytics component in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -32,6 +33,7 @@ export default function RootLayout({
           <main>
             {children}
             <SpeedInsights />
+            <Analytics />
           </main>
           <Toaster richColors />
         </ThemeProvider>
